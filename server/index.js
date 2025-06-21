d1a71f88f5b31b26df33365079eac5610076b3a2
kyrieko <kyrieko1254@gmail.com>
2025-06-21 18:01:36 +0900

    환경 변수 수정 및 포트 설정 변경

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
